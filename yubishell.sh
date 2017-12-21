7580b2b457c09446575f2860704f3dfec366bae0
Cloud Foundry Identity Team <cf-identity-eng@pivotal.io>
2017-12-21 12:16:41 -0800

    Add ~/.ssh/config to yubishell function

--- yubishell.sh
+++ yubishell.sh
@@ -4,6 +4,9 @@ yubishell ()
     if [[ ! -d $HOME/.ssh/controlmasters ]]; then
       mkdir "${HOME}/.ssh/controlmasters"
     fi
+    ssh_config="${HOME}/.ssh/config"
+    ssh_config_url="https://raw.githubusercontent.com/oliverralbertini/scripts/master/ssh_config"
+    grep "Host github.com" "$ssh_config" >/dev/null || curl $ssh_config_url >> $ssh_config
     export GPG_TTY=$(tty);
     export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh";
     export PS1="\[\e[31m\][YUBI]\[\e[0m\] \$ ";
