08a618d93b1ee42d44af276ce9b03cbb81b5a9d6
Cloud Foundry Identity Team <cf-identity-eng@pivotal.io>
2017-12-21 13:51:09 -0800

    Make compound-command of yubigit() a subshell, directly

--- yubigit.sh
+++ yubigit.sh
@@ -1,6 +1,6 @@
-yubigit ()
-{
-    ( gpg-connect-agent --quiet updatestartuptty /bye;
+yubigit()
+(
+    gpg-connect-agent --quiet updatestartuptty /bye;
     if [[ ! -d $HOME/.ssh/controlmasters ]]; then
       mkdir "${HOME}/.ssh/controlmasters"
     fi
@@ -9,9 +9,8 @@ yubigit ()
     grep "Host github.com" "$ssh_config" >/dev/null || curl $ssh_config_url >> $ssh_config
     export GPG_TTY=$(tty);
     export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh";
-    export PS1="\[\e[31m\][YUBI]\[\e[0m\] \$ ";
     if which kr > /dev/null 2>&1; then
         kr unpair;
     fi;
-    ssh github.com )
-}
+    ssh github.com
+)
