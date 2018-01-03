3d9beddb9f5e59ddbc6491b10cd463521e996e32
Cloud Foundry Identity Team <cf-identity-eng@pivotal.io>
2018-01-02 16:54:46 -0800

    Reduce if statements to one line

--- yubigit.sh
+++ yubigit.sh
@@ -1,16 +1,12 @@
 yubigit()
 (
     gpg-connect-agent --quiet updatestartuptty /bye >/dev/null;
-    if [[ ! -d $HOME/.ssh/controlmasters ]]; then
-      mkdir "${HOME}/.ssh/controlmasters";
-    fi;
+    [[ ! -d $HOME/.ssh/controlmasters ]] && mkdir "${HOME}/.ssh/controlmasters";
     ssh_config="${HOME}/.ssh/config";
     ssh_config_url="https://raw.githubusercontent.com/oliverralbertini/scripts/master/ssh_config";
     grep "Host github.com" "$ssh_config" >/dev/null || curl $ssh_config_url >> $ssh_config;
     export GPG_TTY=$(tty);
     export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh";
-    if which kr >/dev/null 2>&1; then
-        kr unpair >/dev/null;
-    fi;
+    which kr >/dev/null 2>&1 && kr unpair >/dev/null;
     ssh github.com
 )
