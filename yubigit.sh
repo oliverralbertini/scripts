938590ed78a3405e21add7d3f758ce6930c195fc
Oliver Albertini <oalbertini@pivotal.io>
2018-01-03 13:14:28 -0800

    Add curly brackets to HOME

--- yubigit.sh
+++ yubigit.sh
@@ -6,7 +6,7 @@ yubigit()
     ssh_config_url="https://raw.githubusercontent.com/oliverralbertini/scripts/master/ssh_config";
     grep "Host github.com" "$ssh_config" >/dev/null || curl "$ssh_config_url" >> "$ssh_config";
     export GPG_TTY=$(tty);
-    export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh";
+    export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh";
     which kr >/dev/null 2>&1 && kr unpair >/dev/null;
     ssh github.com
 )
