04b6ee8b00ee2e613ca2148054ea182edcf2d135
Oliver Albertini <oliver.ruben@gmail.com>
2018-01-02 22:11:28 -0800

    Add quotes around some variables.

--- yubigit.sh
+++ yubigit.sh
@@ -4,7 +4,7 @@ yubigit()
     [[ ! -d $HOME/.ssh/controlmasters ]] && mkdir "${HOME}/.ssh/controlmasters";
     ssh_config="${HOME}/.ssh/config";
     ssh_config_url="https://raw.githubusercontent.com/oliverralbertini/scripts/master/ssh_config";
-    grep "Host github.com" "$ssh_config" >/dev/null || curl $ssh_config_url >> $ssh_config;
+    grep "Host github.com" "$ssh_config" >/dev/null || curl "$ssh_config_url" >> "$ssh_config";
     export GPG_TTY=$(tty);
     export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh";
     which kr >/dev/null 2>&1 && kr unpair >/dev/null;
