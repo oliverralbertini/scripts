3d06234e522159350e37e5150c814579ef3d6636
Cloud Foundry Identity Team <cf-identity-eng@pivotal.io>
2018-01-22 09:13:58 -0800

    Update take-me-to-react script
    * to actually work with/without http:// at the start of the string

--- take-me-to-react.sh
+++ take-me-to-react.sh
@@ -1,2 +1,8 @@
 read url
-echo $url | sed -e "s/8001/3000/" -e "s/dashboard/react-dashboard/"
+url="$(echo $url | sed -e "s/8001/3000/" -e "s/dashboard/react-dashboard/")"
+if [[ $url =~ "http" ]]; then
+  echo "$url"
+else
+  echo "http://$url"
+fi
+# "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" "${url}"
