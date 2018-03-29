14007680174975d51bf054023a22bf8e7584172d
Oliver Albertini <oliver.ruben@gmail.com>
2018-03-29 08:22:05 -0700

    Make yubigit alter the ControlPersist time
    * We only want the tunnel open until end of day

--- yubigit.sh
+++ yubigit.sh
@@ -5,8 +5,18 @@ yubigit()
     ssh_config="${HOME}/.ssh/config";
     ssh_config_url="https://raw.githubusercontent.com/oliverralbertini/scripts/master/ssh_config";
     grep "Host github.com" "$ssh_config" >/dev/null || curl "$ssh_config_url" >> "$ssh_config";
+    local pau_time current_time tunnel_duration;
+    if [[ $(uname) == [Ll]inux ]]; then
+        pau_time="$(date --date='6:00 pm today' +%s)";
+    else
+        pau_time="$(date -j -f "%a %b %d %H:%M:%S %Z %Y" "$(date "+%a %b %d 18:00:00 %Z %Y")" "+%s")";
+    fi;
+    current_time="$(date "+%s")";
+    tunnel_duration=$(( pau_time - current_time ));
+    (( tunnel_duration < 0 )) && tunnel_duration=3600;
+    sed -ie "/Host github.com$/,/Host .*$/ s/^     ControlPersist.*$/     ControlPersist $tunnel_duration/" ~/.ssh/config
     export GPG_TTY=$(tty);
     export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh";
-    which kr >/dev/null 2>&1 && kr unpair >/dev/null;
+    command kr >/dev/null 2>&1 && kr unpair >/dev/null;
     ssh github.com
 )
--- yubishell.sh
+++ yubishell.sh
@@ -2,12 +2,9 @@ yubishell ()
 (
     gpg-connect-agent --quiet updatestartuptty /bye;
     [[ ! -d ${HOME}/.ssh/controlmasters ]] && mkdir "${HOME}/.ssh/controlmasters";
-    ssh_config="${HOME}/.ssh/config";
-    ssh_config_url="https://raw.githubusercontent.com/oliverralbertini/scripts/master/ssh_config"
-    grep "Host github.com" "$ssh_config" >/dev/null || curl "$ssh_config_url" >> "$ssh_config"
     export GPG_TTY=$(tty);
     export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh";
     export PS1="\[\e[31m\][YUBI]\[\e[0m\] \$ ";
-    which kr > /dev/null 2>&1 && kr unpair;
+    command kr > /dev/null 2>&1 && kr unpair;
     bash --norc
 )
