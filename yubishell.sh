129279dead4bcf174775aea1c83685ac67a156d2
Oliver Albertini <oalbertini@pivotal.io>
2018-08-03 13:24:34 -0700

    yubishell runs command given as a parameter

--- yubishell.sh
+++ yubishell.sh
@@ -2,9 +2,16 @@ yubishell ()
 (
     gpg-connect-agent --quiet updatestartuptty /bye;
     [[ ! -d ${HOME}/.ssh/controlmasters ]] && mkdir "${HOME}/.ssh/controlmasters";
-    export GPG_TTY=$(tty);
-    export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh";
-    export PS1="\[\e[31m\][YUBI]\[\e[0m\] \$ ";
+    GPG_TTY=$(tty);
+    SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh";
+    PS1="\[\e[31m\][YUBI]\[\e[0m\] \$ ";
+    export GPG_TTY SSH_AUTH_SOCK PS1
     command kr > /dev/null 2>&1 && kr unpair;
-    bash --norc
+    if [[ ! -z "$*" ]]; then
+        bash --norc -c "$*"
+    else
+        bash --norc
+    fi
 )
+
+yubishell "$*"
