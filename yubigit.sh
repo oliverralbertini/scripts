5ec4f8b2dac37da894e87a5930835b9ce85ea027
Oliver Albertini <oalbertini@pivotal.io>
2018-09-09 17:39:35 -0700

    Update yubi scripts

--- yubigit.sh
+++ yubigit.sh
@@ -1,12 +1,19 @@
-yubigit()
-(
-    gpg-connect-agent --quiet updatestartuptty /bye >/dev/null;
-    [[ ! -d $HOME/.ssh/controlmasters ]] && mkdir "${HOME}/.ssh/controlmasters";
-    ssh_config="${HOME}/.ssh/config";
-    ssh_config_url="https://raw.githubusercontent.com/oliverralbertini/scripts/master/ssh_config";
-    grep "Host github.com" "$ssh_config" >/dev/null || curl "$ssh_config_url" >> "$ssh_config";
-    export GPG_TTY=$(tty);
-    export SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh";
-    command kr >/dev/null 2>&1 && kr unpair >/dev/null;
-    ssh github.com
-)
+#!/usr/bin/env bash
+
+yubigit() {
+  local GPG_TTY SSH_AUTH_SOCK ssh_config ssh_config_url
+  gpgconf --kill gpg-agent
+  gpg-connect-agent --quiet updatestartuptty /bye > /dev/null
+  ! [[ -d $HOME/.ssh/controlmasters ]] && mkdir "${HOME}/.ssh/controlmasters"
+  ssh_config=${HOME}/.ssh/config
+  ssh_config_url=https://raw.githubusercontent.com/oliverralbertini/scripts/master/ssh_config
+  grep --color=auto "Host github.com" "$ssh_config" > /dev/null || \
+    curl "$ssh_config_url" >> "$ssh_config"
+  GPG_TTY=$(tty)
+  SSH_AUTH_SOCK=${HOME}/.gnupg/S.gpg-agent.ssh
+  export GPG_TTY SSH_AUTH_SOCK
+  command kr > /dev/null 2>&1 && kr unpair > /dev/null
+  ssh github.com
+}
+
+yubigit
--- yubishell.sh
+++ yubishell.sh
@@ -1,17 +1,19 @@
-yubishell ()
-(
-    gpg-connect-agent --quiet updatestartuptty /bye;
-    [[ ! -d ${HOME}/.ssh/controlmasters ]] && mkdir "${HOME}/.ssh/controlmasters";
-    GPG_TTY=$(tty);
-    SSH_AUTH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh";
-    PS1="\[\e[31m\][YUBI]\[\e[0m\] \$ ";
-    export GPG_TTY SSH_AUTH_SOCK PS1
-    command kr > /dev/null 2>&1 && kr unpair;
-    if [[ ! -z "$*" ]]; then
-        bash --norc -c "$*"
-    else
-        bash --norc
-    fi
-)
+#!/usr/bin/env bash
+yubishell() {
+  local GPG_TTY SSH_AUTH_SOCK PS1
+  gpgconf --kill gpg-agent
+  gpg-connect-agent --quiet updatestartuptty /bye
+  [[ ! -d ${HOME}/.ssh/controlmasters ]] && mkdir "${HOME}/.ssh/controlmasters"
+  GPG_TTY=$(tty)
+  SSH_AUTH_SOCK=${HOME}/.gnupg/S.gpg-agent.ssh
+  PS1="\\[\\e[31m\\][YUBI]\\[\\e[0m\\] \$ "
+  export GPG_TTY SSH_AUTH_SOCK PS1
+  command kr > /dev/null 2>&1 && kr unpair
+  if [[ -z "$*" ]]; then
+    bash --norc
+  else
+    bash --norc -c "$*"
+  fi
+}
 
 yubishell "$*"
